83d2a19ae5eae753386f70614d40f81580128daa
hdesale@rdalabs.com <hdesale@rdalabs.com>
2023-05-31 16:08:19 +0530

    Commiting for loop package to the BranchX

---
+++ testing-automation-testng-util/src/main/java/loops/ForLoop.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class ForLoop 
+{
+	/*
+	 * For Loop
+	 */
+	
+	
+	public static void main(String[] args) 
+	{
+		for(int i=10; i<=23; i=i+1)
+		{
+			System.out.println(i);
+		}
+		
+	}
+
+}
